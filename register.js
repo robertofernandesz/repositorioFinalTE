49520a8da15fd413e94fa7aa63ed9073d978c3c4
robertofernandesz <roberto.junior3@estudante.ifms.edu.br>
2020-12-17 16:42:25 -0300

    adição do register e do sw

---
+++ register.js
@@ -0,0 +1,6 @@
+if ('serviceWorker' in navigator)
+{
+  navigator.serviceWorker.register('sw.js')
+  .then(function(){ console.log('serviceWorker está registrado') })
+  .catch(function(){ console.log('Erro ao registrar serviceWorker') })
+}
